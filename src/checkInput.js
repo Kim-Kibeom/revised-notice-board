1567f2bf880964f18a544c44c9713e60b82c2704
quetal <gibum1995@gmail.com>
2021-09-12 22:55:02 +0900

    Feat: execute search function

--- src/checkInput.js
+++ src/checkInput.js
@@ -1,8 +1,8 @@
 function check() {
-    // var formData = $("#myform").serialize();
     const isChecked = document.getElementsByName("ckb");
     const theForm = document.frm;
     let flag = false;
+    const getCheck = [];
 
     if (theForm.input.value == "") {
         alert("키워드를 입력하세요.");
@@ -19,4 +19,10 @@ function check() {
         alert("원하는 공지사항을 체크하세요.");
         return;
     }
+
+    for (let i = 0; i < isChecked.length; i++) {
+        getCheck.push(isChecked[i].checked);
+    }
+
+    getResult(getCheck, theForm.input.value);
 }
